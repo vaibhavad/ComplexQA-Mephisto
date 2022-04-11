2c9dd0a35c3dc9b71021a945ca68bc21d92f9a1d
Vaibhav Adlakha <vaibhavadlakha95@gmail.com>
2022-04-11 00:56:39 -0400

    set initial amount earned

--- packages/bootstrap-chat/src/composed/ChatApp.jsx
+++ packages/bootstrap-chat/src/composed/ChatApp.jsx
@@ -172,6 +172,7 @@ function ChatApp({
     if (agentId) {
       console.log("connecting...");
       connect(agentId);
+      setAmountEarned(taskConfig.task_reward_base);
     }
   }, [agentId]);
 
