89723ef0e09958147b74bd2fc3ebc8374bd14555
Nicholas Meade <nicholas.c.meade@gmail.com>
2022-04-11 13:27:03 -0400

    Add production qualification test

--- qualification-test/create_qualification.py
+++ qualification-test/create_qualification.py
@@ -3,7 +3,8 @@ import sys
 import os
 
 
-endpoint_url = "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
+# endpoint_url = "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
+endpoint_url = "https://mturk-requester.us-east-1.amazonaws.com"
 
 print(f"Deploying to {endpoint_url}.")
 
@@ -19,8 +20,8 @@ mturk = boto3.client("mturk", region_name="us-east-1", endpoint_url=endpoint_url
 
 try:
     qual_response = mturk.create_qualification_type(
-        Name="Complex QA v0.0.24",
-        Keywords="question,answer,dialog",
+        Name="Complex QA v0.1.0",
+        Keywords="question,answer,complex,dialog,conversation,reasoning",
         Description="This is a brief qualification test showing how to form complex questions from dialogues. You can take it multiple times.",
         QualificationTypeStatus="Active",
         Test=questions,
